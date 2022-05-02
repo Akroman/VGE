f4237765299f698db9b14d498e48064d19584a8a
Miroslav Karpíšek <miro@dhcpe105.fit.vutbr.cz>
2022-05-02 12:05:59 +0200

    monotone polygon greedy triangulation

--- src/Triangulator.js
+++ src/Triangulator.js
@@ -98,107 +98,97 @@ export default class Triangulator {
 
     greedyMonotoneTriangulate() {
         const pointsCount = this.polygon.allPoints.length;
-        const leftPoint = this.findMostLeftPoint();
+        console.log('points count', pointsCount);
 
         this.triangles = [];
-        this.Q = [];
         this.animatedtriangles = [];
-        this.usedPoints = [];
-        
-        var lastTop, lastBot;
-        var topPoint, botPoint, _;
-        var currentPoint;
-        var lastWasTop = true;
 
-        const leftNode = this.polygon.getNodeFromPoint(leftPoint);
-        [topPoint, botPoint] = Utils.getPreviousAndNextPoint(leftNode);
+        const origPolygon = this.polygon.clone();
+        this.sortPoints2();
 
-        var topNode, botNode;
-        lastTop = leftPoint;
-        lastBot = botPoint;
+        this.Q = [];
+        this.stackPaths = [];
 
-        //this.triangles.push(new Triangle(leftPoint, topPoint, botPoint));
+        var p1 = this.xSorted.shift();
+        var p1_p = this.topPath.shift();
 
-        this.Q.push(botPoint);
-        this.Q.push(leftPoint);
-        
+        var p2 = this.xSorted.shift();
+        var p2_p = this.topPath.shift();
 
-        var goingTop = false;
+        this.Q.push(p1);
+        this.Q.push(p2);
 
-        while(this.triangles.length < pointsCount - 2) {
-            lastWasTop = goingTop;
+        var prevTop = p2_p;
 
-            topNode = this.polygon.getNodeFromPoint(lastTop);
-            botNode = this.polygon.getNodeFromPoint(lastBot);
+        for(var i = 2; i < pointsCount; i++) {
+            var p_i = this.xSorted.shift();
+            var p_i_p = this.topPath.shift();
 
-            [topPoint, _] = Utils.getPreviousAndNextPoint(topNode);
-            [_, botPoint] = Utils.getPreviousAndNextPoint(botNode);
+            console.log(p_i_p, prevTop);
+            var same_path = (p_i_p == prevTop);
 
-            if (topPoint.x < botPoint.x) {
-                botPoint = lastBot;
-                goingTop = true;
-                currentPoint = topPoint;
-            }
-            else {
-                topPoint = lastTop;
-                goingTop = false;
-                currentPoint = botPoint;
-            }
+            prevTop = p_i_p;
 
-            if (this.usedPoints.length > pointsCount) {
-                console.log('error');
-                break;
-            }
+            if (same_path) {
+                this.Q.push(p_i);
 
-            console.log(this.triangles, this.Q, lastWasTop, goingTop, currentPoint);
-
-            // same chain
-            if (lastWasTop == goingTop) {
-                this.Q.push(currentPoint);
-                while (this.Q.length >= 3) {
+                // lezi na stejne strane 
+                while(this.Q.length >= 3) {
                     var last1 = this.Q.pop();
                     var last2 = this.Q.pop();
+                    var last3 = this.Q.pop();
 
-                    console.log(last2, last1, currentPoint, this.find_angle(last2, last1, currentPoint));
+                    if (p_i_p) {
+                        // jdu vrchem
+                        if (this.isAbove(last3, last1, last2)) {
+                            this.triangles.push(new Triangle(last1, last2, last3));
 
-                    if (this.find_angle(last2, last1, currentPoint) < 180) {
-                        this.triangles.push(new Triangle(last2, last1, currentPoint));
-                        this.Q.push(last2);
+                            this.Q.push(last3);
+                            this.Q.push(last1);
+                        }
+                        else {
+                            this.Q.push(last3);
+                            this.Q.push(last2);
+                            this.Q.push(last1);
+                            break;
+                        }
+
                     }
                     else {
-                        this.Q.push(last2);
-                        this.Q.push(last1);
+                        // jdu spodem
 
-                        console.log('break same chain,', this.Q);
-                        break;
+                        if (this.isAbove(last3, last1, last2)) {
+                            this.triangles.push(new Triangle(last1, last2, last3));
+                            this.Q.push(last3);
+                            this.Q.push(last1);
+                        }
+                        else {
+                            this.Q.push(last3);
+                            this.Q.push(last2);
+                            this.Q.push(last1);
+                            break;
+                        }
                     }
-
-                    console.log('same chain,', this.Q);
                 }
             }
-            // different chain
             else {
-                while (this.Q.length >= 2) {
-                    var last1 = this.Q.pop();
-                    var last2 = this.Q.pop();
-                    
-                    this.triangles.push(new Triangle(last2, last1, currentPoint));
+                //this.Q.push(p_i);
 
-                    this.Q.push(last2);
+                // lezi na opacne strane
+                // uhlopricky pro vsechny ulozene body
+                while(this.Q.length >= 2) {
+                    console.log('tada');
+                    var last2 = this.Q.shift();
+                    var last1 = this.Q.shift();
+
+                    this.triangles.push(new Triangle(last1, p_i, last2));
+                    
+                    this.Q.unshift(last1);
                 }
 
-                this.Q.push(currentPoint);
-
-                console.log('different chain,', this.Q);
+                this.Q.push(p_i);
             }
-
-            console.log('triangles length', this.triangles.length);
-
-            this.usedPoints.push(currentPoint);
-            console.log(this.usedPoints.length);
-
-            lastTop = topPoint;
-            lastBot = botPoint;
+            console.log(this.triangles.length);
         }
     }
 
@@ -258,5 +248,126 @@ export default class Triangulator {
 
     toDegrees(radians) {
         return 360 * radians / (2 * Math.PI)
+    }
+
+    sortPoints() {
+        const origPolygon = this.polygon.clone();
+        const pointsCount = this.polygon.allPoints.length;
+
+        this.xSorted = []
+        this.samePaths = [] // True if going over top, Fals if going over bot 
+
+        const leftPoint = this.findMostLeftPoint();
+        
+
+        while (this.xSorted.length < pointsCount) {
+            var leftestPoint = this.polygon.allPoints.shift();
+
+            if (this.polygon.allPoints.length > 0) {
+                console.log('here');
+                for (var i = 0; i < this.polygon.allPoints.length + 1; i++) {
+                    var currentPoint = this.polygon.allPoints.shift();
+
+                    if (currentPoint.x < leftestPoint.x) {
+                        this.polygon.allPoints.push(leftestPoint);
+                        leftestPoint = currentPoint;
+                    }
+                    else {
+                        this.polygon.allPoints.push(currentPoint);
+                    }
+                }
+            }
+            else { 
+                this.polygon.allPoints.push(leftestPoint);
+            }
+
+            console.log('leftest', leftestPoint);
+           
+            if (this.xSorted.length == 0) {
+                this.xSorted.push(leftestPoint);
+                this.samePaths.push(true);
+            }
+            else {
+                var previousPoint = this.xSorted.pop();
+
+                var testNode = origPolygon.getNodeFromPoint(previousPoint);
+                var [prevPoint, nextPoint] = Utils.getPreviousAndNextPoint(testNode);
+
+                if (leftestPoint == nextPoint) {
+                    // they are on the same path
+                    this.xSorted.push(previousPoint);
+                    this.xSorted.push(leftestPoint);
+
+                    this.samePaths.push(true);
+                }
+                else if (leftestPoint == prevPoint) {
+                    // they are on the same path
+                    this.xSorted.push(previousPoint);
+                    this.xSorted.push(leftestPoint);
+
+                    this.samePaths.push(false);
+                }
+                else {
+                    // they are on diff path
+                    this.xSorted.push(previousPoint);
+                    this.xSorted.push(leftestPoint);
+                    
+                    var last = this.samePaths.pop();
+                    this.samePaths.push(last);
+                    this.samePaths.push(!last);
+                }
+            }
+
+            console.log('points lenght', this.polygon.allPoints);
+        }
+
+        console.log('sorted', this.xSorted);
+        console.log('samePathsAsPrev', this.samePaths)
+        
+    }
+
+    sortPoints2 () {
+        const origPolygon = this.polygon.clone();
+        const pointsCount = this.polygon.allPoints.length;
+
+        this.xSorted = []
+        this.topPath = []; // True if going over top, Fals if going over bot 
+        
+        const leftPoint = this.findMostLeftPoint();
+
+        var lastTop = leftPoint;
+        var lastBot = leftPoint;
+
+        this.topPath.push(true);
+        this.xSorted.push(leftPoint);
+
+        while (this.xSorted.length < pointsCount) {
+            var topNode = this.polygon.getNodeFromPoint(lastTop);
+            var botNode = this.polygon.getNodeFromPoint(lastBot);
+
+            var [topPoint, _] = Utils.getPreviousAndNextPoint(topNode);
+            var [_, botPoint] = Utils.getPreviousAndNextPoint(botNode);
+
+            if(topPoint.x < botPoint.x) {
+                // jdu horni cestou
+
+                lastTop = topPoint;
+                this.topPath.push(true);
+                this.xSorted.push(topPoint);
+            } 
+            else {
+                // jdu dolni cestou
+
+                lastBot = botPoint;
+                this.topPath.push(false);
+                this.xSorted.push(botPoint);
+            }
+
+            console.log('points lenght', this.xSorted);
+        }
+
+        console.log('sorted', this.xSorted);
+        console.log('samePathsAsPrev', this.topPath)
+        
     }
 }
